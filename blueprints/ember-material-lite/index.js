3e7dcd65f3893114098f4e3a96c51359e6838f76
Adam Haris <iamadamharis@gmail.com>
2018-03-12 09:08:30 +0100

    update blueprint deps

--- blueprints/ember-material-lite/index.js
+++ blueprints/ember-material-lite/index.js
@@ -16,8 +16,8 @@ module.exports = {
       this.addPackageToProject('ember-cli-sass', '^5.3.1'),
       this.addPackageToProject('ember-cli-autoprefixer', '~0.6.0'),
       this.addPackageToProject('ember-new-computed', '^1.0.3'),
-      this.addPackageToProject('ember-truth-helpers', '~1.2.0'),
-      this.addPackageToProject('ember-composability', '~0.3.0'),
+      this.addPackageToProject('ember-truth-helpers', '~2.0.0'),
+      this.addPackageToProject('ember-composability', '^0.3.8'),
     ]);
   }
 };
